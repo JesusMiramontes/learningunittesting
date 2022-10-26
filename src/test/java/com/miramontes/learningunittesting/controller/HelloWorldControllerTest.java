63ccb2c9b2e2179a41a847fe5bc851511c675fe6
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-10-26 16:41:50 -0500

    MockMvc: Verify response status

--- src/test/java/com/miramontes/learningunittesting/controller/HelloWorldControllerTest.java
+++ src/test/java/com/miramontes/learningunittesting/controller/HelloWorldControllerTest.java
@@ -1,6 +1,7 @@
 package com.miramontes.learningunittesting.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ class HelloWorldControllerTest {
                 MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON);
 
         // Store response
-        MvcResult result = mockMvc.perform(request).andReturn();
+        MvcResult result = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
 
         // Verify response
         assertEquals("Hello world.", result.getResponse().getContentAsString());
