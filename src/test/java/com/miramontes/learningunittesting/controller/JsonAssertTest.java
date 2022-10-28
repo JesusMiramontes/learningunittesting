7502d208ed55d4ae8221ef622682e480f25b3874
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-10-27 19:26:47 -0500

    Prepare files for test

---
+++ src/test/java/com/miramontes/learningunittesting/controller/JsonAssertTest.java
@@ -0,0 +1,6 @@
+package com.miramontes.learningunittesting.controller;
+
+public class JsonAssertTest {
+    private static final String expected =
+            "{\"id\":1,\"name\":\"Foo\",\"price\":10,\"quantity\":100}";
+}
