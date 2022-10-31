cc1742a6e11e11b61040b959f9a82c9e2ff1a947
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-10-31 16:32:42 -0600

    MockBean: Inject mock bean, test service layer.

--- src/test/java/com/miramontes/learningunittesting/controller/ItemControllerTest.java
+++ src/test/java/com/miramontes/learningunittesting/controller/ItemControllerTest.java
@@ -1,10 +1,15 @@
 package com.miramontes.learningunittesting.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+import com.miramontes.learningunittesting.business.ItemService;
+import com.miramontes.learningunittesting.model.Item;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -15,6 +20,14 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 class ItemControllerTest {
 
     @Autowired private MockMvc mockMvc;
+
+    @MockBean private ItemService itemServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        when(itemServiceMock.retrieveHardcodedItem())
+                .thenReturn(Item.builder().id(1).name("Bar").price(200).quantity(200).build());
+    }
 
     @Test
     void dummyItem() throws Exception {
@@ -27,6 +40,17 @@ class ItemControllerTest {
                                         .json(
                                                 "{ \"id\": 1, \"name\": \"Foo\", \"price\": 10,"
                                                         + " \"quantity\": 100 }"))
+                        .andReturn();
+    }
+
+    @Test
+    void itemFromService() throws Exception {
+        RequestBuilder request =
+                MockMvcRequestBuilders.get("/item-from-service-layer")
+                        .accept(MediaType.APPLICATION_JSON);
+        MvcResult result =
+                mockMvc.perform(request)
+                        .andExpect(content().json("{id:1, name:Bar, price:200, quantity:200}"))
                         .andReturn();
     }
 }
