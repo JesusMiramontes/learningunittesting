4147e7c20b839764fa7320c2a5c14507c629a02f
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-10-31 17:31:03 -0600

    Insert values to db.

--- src/main/java/com/miramontes/learningunittesting/model/Item.java
+++ src/main/java/com/miramontes/learningunittesting/model/Item.java
@@ -1,17 +1,22 @@
 package com.miramontes.learningunittesting.model;
 
+import javax.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+@Entity
 @Getter
 @Setter
 @Builder
 @ToString
 public class Item {
-    private int id;
+    @Id private int id;
+
     private String name;
     private int price;
     private int quantity;
+
+    @Transient private int value;
 }
