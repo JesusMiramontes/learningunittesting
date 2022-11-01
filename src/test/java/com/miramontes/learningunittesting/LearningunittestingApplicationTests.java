f124f1915aace7df3d4273a0089c293b28d8c11d
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-10-31 20:25:48 -0600

    @SpringBootTest: Simple Integration Test.

--- src/test/java/com/miramontes/learningunittesting/LearningunittestingApplicationTests.java
+++ src/test/java/com/miramontes/learningunittesting/LearningunittestingApplicationTests.java
@@ -1,11 +1,20 @@
 package com.miramontes.learningunittesting;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LearningunittestingApplicationTests {
 
+    @Autowired private TestRestTemplate testRestTemplate;
+
     @Test
-    void contextLoads() {}
+    void allItemsPresentFromDb() throws JSONException {
+        String response = this.testRestTemplate.getForObject("/all-items-from-db", String.class);
+        JSONAssert.assertEquals("[{id:10001}, {id:10002}, {id:10003}]", response, false);
+    }
 }
