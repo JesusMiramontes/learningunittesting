332de314319183f45e86b9a3bacfe5c29e98ef1e
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-10-31 20:25:21 -0600

    Spotless.

--- src/test/java/com/miramontes/learningunittesting/data/ItemRepositoryTest.java
+++ src/test/java/com/miramontes/learningunittesting/data/ItemRepositoryTest.java
@@ -1,24 +1,21 @@
 package com.miramontes.learningunittesting.data;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.miramontes.learningunittesting.model.Item;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
 class ItemRepositoryTest {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    @Autowired private ItemRepository itemRepository;
 
     @Test
-    void findAll_size(){
+    void findAll_size() {
         List<Item> itemRepositoryAll = itemRepository.findAll();
         assertEquals(3, itemRepositoryAll.size());
     }
-
 }
